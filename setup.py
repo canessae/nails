e6fbce77d05aedf3b631800d1e986a410b48b842
Livius Tenze <ltenze@gmail.com>
2024-08-18 10:35:32 +0200

    Fix setup... to be tested

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     license='GPL-3.0 license',
     author='ICTP',
     author_email='ltenze@gmail.com',
-    description='We present an algorithm that allows to identify color variations from fingernails using Artificial Intelligence (A.I.). Healthy nails have a uniform color and are visually smooth. However, as one ages, nails may become more brittle and may have discoloration due to injury, fungal and viral infections, medications, etc. Furthermore, the appearance of the fingernails can also change due to some medical conditions.'
+    description='We present an algorithm that allows to identify color variations from fingernails using Artificial Intelligence (A.I.). Healthy nails have a uniform color and are visually smooth. However, as one ages, nails may become more brittle and may have discoloration due to injury, fungal and viral infections, medications, etc. Furthermore, the appearance of the fingernails can also change due to some medical conditions.',
     install_requires = [
         'timm==0.4.12',
         'segmentation-models-pytorch==0.3.0',
